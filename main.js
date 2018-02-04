216b63546813f66833c7cbb966209d591fdb568c
emmabennett <emma.j.bennett1015@gmail.com>
2018-02-04 14:34:01 -0800

    html, basic style and psuedocode for JS

---
+++ main.js
@@ -0,0 +1,7 @@
+// all items in cart
+// cereal items + pics
+// candy + pics
+// juices + pics
+// make the buttons click to page of type
+// make the pictures click
+// add the food to the shopping cart
