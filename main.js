a66716029c56ad9c91df099aef1f258f5c8d56b6
emmabennett <emma.j.bennett1015@gmail.com>
2018-02-04 15:16:46 -0800

    set pictures and basic click functions in JS

--- main.js
+++ main.js
@@ -1,7 +1,96 @@
 // all items in cart
+var shoppingcart = [];
+
 // cereal items + pics
+var cerealItems = [
+	{
+		name: "cheerios",
+		img: "./wdi_images/cheerios.jpg"
+	},
+	{
+		name: "fruit_loops",
+		img: "./wdi_images/fruit-loops.jpg"
+	},
+	{
+		name: "puffs",
+		img: "./wdi_images/puffs.jpg"
+	},
+	{
+		name: "rainen bran",
+		img: "./wdi_images/raisen-bran.jpg"
+	},
+	{
+		name: "crunch",
+		img: "./wdi_images/crunch.jpg"
+	}
+
+];
+
 // candy + pics
+
+var candyItems = [
+	{
+		name: "kit-kats",
+		img: "./wdi_images/kit-kat.jpg"
+	},
+	{
+		name: "mnms",
+		img: "./wdi_images/m&ms.jpg"
+	},
+	{
+		name: "skittles",
+		img: "./wdi_images/skittles.jpg"
+	},
+	{
+		name: "snickers",
+		img: "./wdi_images/snickers.jpg"
+	},
+	{
+		name: "twixx",
+		img: "./wdi_images/twix.jpg"
+	}
+
+];
+
 // juices + pics
+
+var juiceItems = [
+	{
+		name: "apple",
+		img: "./wdi_images/apple-juice.jpg"
+	},
+	{
+		name: "carrot",
+		img: "./wdi_images/carrot-juice.jpg"
+	},
+	{
+		name: "grape",
+		img: "./wdi_images/grape-juice.jpeg"
+	},
+	{
+		name: "orange",
+		img: "./wdi_images/orange-juice.jpg"
+	},
+	{
+		name: "tomato",
+		img: "./wdi_images/tomato-juice.jpg"
+	}
+
+
+];
 // make the buttons click to page of type
-// make the pictures click
-// add the food to the shopping cart
+$("#cereal").click(function() {
+	addColumn1("cereal");
+});
+
+$("#candy") .click(function() {
+	addColumn1("candy");
+});
+
+$("#juices").click(function() {
+	addColumn1("juices");
+});
+// make the pictures click column1
+
+
+// add the food to the shopping cart column 2
